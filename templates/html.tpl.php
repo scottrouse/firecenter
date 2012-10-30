eca4608440233da649e36bb82f0c05aadf707d3d
Scott Rouse <scottwrouse@gmail.com>
2012-10-29 22:13:25 -0600

    HTML for UM Header

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -104,6 +104,42 @@
       <a href="#<?php print $skip_link_anchor; ?>" class="element-invisible element-focusable"><?php print $skip_link_text; ?></a>
     </p>
   <?php endif; ?>
+  
+  <!-- Begin University of Montana Header Code. Adapted from http://umt.edu/webcommittee/standards/design.aspx -->
+  <div id="um-header">
+    <div id="um-logo">
+      <a href="http://www.umt.edu/" title="The University of Montana"><img alt="The University of Montana" src="http://jour.umt.edu/sites/all/themes/jschool/images/um/umlogo200w.png" title="UM Logo"></a> <!-- @todo — Replace hard link to UM logo with image replacement. -->
+    </div>
+    <div id="um-topnav">
+        <nav id="navrightlinks_wrap">
+            <ul>
+                <li class="first odd">
+                    <a href="http://www.umt.edu/home/atoz/">A to Z Index</a>
+                </li>
+                <li class="even">
+                    <a href="http://www.umt.edu/home/directory/">Directory</a>
+                </li>
+                <li class="last odd">
+                    <a href="http://www.umt.edu/">UM Home</a>
+                </li>
+            </ul>
+        </nav>
+        <div id="um-search">
+            <form accept-charset="utf-8" action="http://www.umt.edu/home/search/" id="cse-search-box" method="get">
+                <label for="q">
+                    Search UM
+                </label>
+                <input class="gradient_button search_input" id="q" name="q" onblur="resetText(this);" onfocus="clearBox(this);" placeholder="Search UM" value="Search UM" type="text">
+                <input name="cx" value="004842374792843146445:2r-2xi1nlr4" type="hidden">
+                <input name="cof" value="FORID:10" type="hidden">
+                <input name="ie" value="UTF-8" type="hidden"><input class="gradient_button search_button" name="sa" value="Go" type="submit">
+            </form>
+        </div> <!-- /#um-search -->
+    </div> <!-- /#um-topnav -->
+  </div> <!-- /#um-header -->
+  <!-- End University of Montana Header Code -->
+
+
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
